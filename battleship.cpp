881f67321d9eb2c753d60ed182895745bc984e39
davidenkovic <davidenkovic100@gmail.com>
2019-05-21 11:00:39 +0200

    4 to goo bto

--- battleship.cpp
+++ battleship.cpp
@@ -11,5 +11,48 @@
  *-----------------------------------------------------------------------------
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "battleship.h"
+
+CellContent my[FIELDSIZE][FIELDSIZE];
+CellContent op[FIELDSIZE][FIELDSIZE];
+CellContent gu[FIELDSIZE][FIELDSIZE];
+
+
+void load_game(){
+  FILE* fd_my = fopen("battleship.my", "r");
+  for (int i = 0; i < FIELDSIZE; i++) {
+    for (int j = 0; j < FIELDSIZE; j++) {
+      fread(&my[i][j],sizeof(CellContent),1,fd_my);
+    }
+  }
+  FILE* fd_op = fopen("battleship.op", "r");
+  for (int x = 0; x < FIELDSIZE; x++) {
+    for (int y = 0; y < FIELDSIZE; y++) {
+      fread(&op[x][y],sizeof(CellContent),1,fd_op);
+    }
+  }
+  for (int a = 0; a < FIELDSIZE; a++) {
+    for (int b = 0; b < FIELDSIZE; b++) {
+      gu[a][b] = Unknown;
+    }
+  }
+  fclose(fd_op);
+  fclose(fd_my);
+}
+
+CellContent get_shot(int row, int col){
+  if (row<FIELDSIZE && row>-1 && col<FIELDSIZE && col >-1) {
+    return my[row][col];
+  }
+  return OutOfRange;
+}
+
+bool shoot(int row, int col){
+  return true;
+}
+
+CellContent get_my_guess(int row, int col){
+  return gu[row][col];
+}
